1329abcdb8d2ab3bc017d5b0606995f9e2983f72
paty-oliveira <patriciia.mota@gmail.com>
2026-01-21 10:37:40 +0000

    Connect local environment with localstack container

--- terraform.tf
+++ terraform.tf
@@ -1,5 +1,25 @@
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "6.28.0"
+    }
+  }
+}
+
+
 provider "aws" {
-  access_key = "test"
-  secret_key = "test"
-  region     = "eu-central-1"
+  access_key = var.aws_access_key
+  secret_key = var.aws_secret_key
+  region     = var.aws_region
+
+  skip_credentials_validation = true
+  skip_requesting_account_id  = true
+  skip_region_validation      = true
+  skip_metadata_api_check     = true
+  s3_use_path_style           = true
+
+  endpoints {
+    s3 = "http://localhost:4566"
+  }
 }
--- variables.tf
+++ variables.tf
@@ -1,3 +1,15 @@
+variable "aws_access_key" {
+  default = "test"
+}
+
+variable "aws_secret_key" {
+  default = "test"
+}
+
+variable "aws_region" {
+  default = "eu-central-1"
+}
+
 variable "s3_bucket_name" {
   default = "my-test-bucket"
 }
